0b619de7aedfab837906b730b21248c1615bb565
Deepika Gopala <48517167+DeepikaGopalaOSF@users.noreply.github.com>
2019-03-20 12:55:11 +0530

    4706 - js event handler code

--- app_storefront_training/cartridge/js/pages/cart.js
+++ app_storefront_training/cartridge/js/pages/cart.js
@@ -39,6 +39,11 @@ function initializeEvents() {
         }
     });
 
+     //click to view coupon code - 14706
+     $('#click-here-to-code').on('click', function (e) {
+        $(this).hide();
+        $(".cart-coupon-code").css("display","block");
+    })
 }
 
 exports.init = function () {
